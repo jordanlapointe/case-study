a0e0326e45d90be1249c61885837bd9a51f271f9
Jordan LaPointe <jordan.lapointe@bestbuy.com>
2017-09-17 18:16:00 -0500

    fix failing example test

--- src/ProductDetailsPage.js
+++ src/ProductDetailsPage.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react'
 import './ProductDetailsPage.css'
+import _ from 'lodash'
 import ItemHighlights from './ItemHighlights'
 import ItemPhotos from './ItemPhotos'
 import ItemPricing from './ItemPricing'
@@ -11,18 +12,21 @@ const { itemData } = window
 
 export default class ProductDetailsPage extends PureComponent {
   render() {
-    const highlights = itemData.ItemDescription[0].features
-    const images = itemData.Images[0]
-    const pricing = itemData.Offers[0].OfferPrice[0]
-    const reviews = itemData.CustomerReview[0]
+    const highlights = _.get(itemData, 'ItemDescription[0].features')
+    const images = _.get(itemData, 'Images[0]')
+    const pricing = _.get(itemData, 'Offers[0].OfferPrice[0]')
+    const reviews = _.get(itemData, 'CustomerReview[0]')
+    const title = _.get(itemData, 'title')
+
+    if (!itemData) return null
 
     return (
       <main className="ProductDetailsPage">
-        <h1>{ itemData.title }</h1>
-        <ItemPhotos images={ images } />
-        <ItemPricing pricing={ pricing } />
-        <ItemHighlights highlights={ highlights }  />
-        <ReviewsSummary reviews={ reviews } />
+        { title && <h1>{ itemData.title }</h1> }
+        { images && <ItemPhotos images={ images } /> }
+        { pricing && <ItemPricing pricing={ pricing } /> }
+        { highlights && <ItemHighlights highlights={ highlights }  /> }
+        { reviews && <ReviewsSummary reviews={ reviews } /> }
       </main>
     )
   }
